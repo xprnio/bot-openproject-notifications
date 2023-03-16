d6a017ad793a3dc935e87aabe67e97d33f03adfe
Ragnar Laud <ragnar.laud@hotmail.com>
2023-03-16 17:21:36 +0200

    Fix fetch in api.mjs

--- src/lib/open-project/api.mjs
+++ src/lib/open-project/api.mjs
@@ -3,8 +3,7 @@ export { apiFetch as fetch } from './fetch.mjs';
 export { apiFetchLink as link } from './fetch.mjs';
 
 export async function getWorkPackageActivities(id) {
-	const res = await fetch(`/work_packages/${id}/activities`);
-	return await res.json()
+	return await fetch(`/work_packages/${id}/activities`)
 		.then(json => json['_embedded'])
 		.then(json => json['elements']);
 }
